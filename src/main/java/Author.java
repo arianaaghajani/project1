36d879359697ecf5cac41beaf684a2f110ded588
arianaaghajani <aghajaniariana@gmail.com>
2024-01-10 17:45:41 +0330

    write objects and constructor

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -1,2 +1,13 @@
 public class Author{
+    private String name;
+    private String lastName;
+    private int age;
+    private String[] books;
+
+    public Author(String name, String lastName, int age, String[] books) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.books = books;
+    }
 }
