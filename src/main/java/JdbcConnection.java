3f78ee61935c26ba00de84ccdac34ee698734137
arianaaghajani <aghajaniariana@gmail.com>
2024-01-10 23:29:59 +0330

    add methods of connection and getter

--- src/main/java/JdbcConnection.java
+++ src/main/java/JdbcConnection.java
@@ -1,2 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+
+    private final Connection connection=
+            DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgresql","1399ar");
+
+    public JdbcConnection() throws SQLException {
+    }
+
+    public Connection getConnection(){
+        return connection;
+
+    }
 }
