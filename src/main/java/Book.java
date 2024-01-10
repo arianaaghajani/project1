dad343a61e44c56ed018130ef1589022ea3a4cea
arianaaghajani <aghajaniariana@gmail.com>
2024-01-10 18:05:05 +0330

    write objects and constructor

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,3 +1,13 @@
+import javax.xml.crypto.Data;
+
 public class Book {
-    
+    private String title;
+    private Data printYear;
+    private String author;
+
+    public Book(String title, Data printYear, String author) {
+        this.title = title;
+        this.printYear = printYear;
+        this.author = author;
+    }
 }
