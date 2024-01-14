921b80ba7094e6218b75383709c1ed5d47b8a6dd
arianaaghajani <aghajaniariana@gmail.com>
2024-01-14 10:43:27 +0330

    create constructor

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -7,56 +7,23 @@ public class Author{
     private int age;
     private book[] books;
 
-
-
-    public String getName() {
-        return name;
+    public Author() {
     }
 
-    public void setName(String name) {
+    public Author(String name, String lastName, int age, book[] books) {
         this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
         this.age = age;
-    }
-
-    public Stri[] getBooks() {
-        return books;
-    }
-
-    public void setBooks(String[] books) {
         this.books = books;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public Author(int id, String name, String lastName, int age, book[] books) {
         this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.books = books;
     }
 
-    @Override
-    public String toString() {
-        return "Author{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                ", books=" + Arrays.toString(books) +
-                '}';
-    }
+
 }
