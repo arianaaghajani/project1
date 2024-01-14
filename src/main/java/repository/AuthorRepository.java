91132633e724d715bb8aa8d2215f0b68e38bfc37
arianaaghajani <aghajaniariana@gmail.com>
2024-01-14 17:49:46 +0330

    add connection and jdbc connection

--- src/main/java/repository/AuthorRepository.java
+++ src/main/java/repository/AuthorRepository.java
@@ -1,7 +1,13 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class AuthorRepository {
-    public void save(){
+    private final JdbcConnection JdbcConnection= new JdbcConnection();
+    private final Connection connection=JdbcConnection.getConnection();
+
+    public AuthorRepository() throws SQLException{
 
     }
 }
