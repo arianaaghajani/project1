c2913cc12fc427e61fbf63fd5e46a9600782197e
arianaaghajani <aghajaniariana@gmail.com>
2024-01-14 18:16:11 +0330

    method of Author save

--- src/main/java/repository/AuthorRepository.java
+++ src/main/java/repository/AuthorRepository.java
@@ -1,13 +1,23 @@
 package repository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AuthorRepository {
-    private final JdbcConnection JdbcConnection= new JdbcConnection();
-    private final Connection connection=JdbcConnection.getConnection();
+    private final JdbcConnection JdbcConnection = new JdbcConnection();
+    private final Connection connection = JdbcConnection.getConnection();
 
-    public AuthorRepository() throws SQLException{
+    public AuthorRepository() throws SQLException {
 
+    }
+
+    public int save(Author author) throws SQLException {
+        String addAuthor = "INSERT INTO author(name,lastName,age)VALUES(?,?,?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(addAuthor);
+        preparedStatement.setString(1, author.getName());
+        preparedStatement.setString(2, author.getLastName());
+        preparedStatement.setInt(3, author.getAge());
+        return preparedStatement.executeUpdate();
     }
 }
