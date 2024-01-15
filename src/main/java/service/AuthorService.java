13aad80897620432d7c97c87f38a5fb28be7e314
arianaaghajani <aghajaniariana@gmail.com>
2024-01-15 11:16:10 +0330

    method of register author

--- src/main/java/service/AuthorService.java
+++ src/main/java/service/AuthorService.java
@@ -12,4 +12,19 @@ public class AuthorService {
 
     public AuthorService() throws SQLException {
     }
+    public void register()throws SQLException{
+        System.out.println("Enter an author name: ");
+        String name=scanner.nextLine();
+        System.out.println("Enter a lastName of author: ");
+        String lastName=scanner.nextLine();
+        System.out.println("Enter an Age: ");
+        String age=scanner.nextLine();
+        scanner.nextInt();
+        Author author=new Author(name,lastName,age);
+        int resulte=AuthorRepository.save(author);
+        if (resulte==1)
+            System.out.println("The author is register");
+        else
+            System.out.println("error");
+    }
 }
