d960508541f66e02b88a869204cb38d71556e037
arianaaghajani <aghajaniariana@gmail.com>
2024-01-15 12:01:40 +0330

    method of addBook

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -10,4 +10,15 @@ public class BookService {
     private final Scanner scanner=new Scanner(System.in);
     public BookService() throws SQLException {
     }
+    public void addBook()throws SQLException{
+        System.out.println("enter a title of book");
+        String title =scanner.nextLine();
+        System.out.println("enter a year of book");
+        int year=scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("enter authorId of book");
+        int authorId=scanner.nextInt();
+        scanner.nextLine();
+
+    }
 }
