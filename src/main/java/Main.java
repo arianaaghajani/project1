5f71023c966bf7f5615ac0f962b2a41a43a86621
arianaaghajani <aghajaniariana@gmail.com>
2024-01-15 11:27:34 +0330

    create Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,2 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-
 public class Main {
-    public static void main(String[] args) throws SQLException{
-    Connection connection=
-    DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","1399ar");
-        System.out.println(connection);
-        String createTable ="CREATE TABLE users (ID serial PRIMARY KEY,username VARCHAR(50) UNIQUE NOT NULL);";
-        PreparedStatement preparedStatement= connection.prepareStatement(createTable);
-        System.out.println(preparedStatement.execute());
-
-    }
-
 }
