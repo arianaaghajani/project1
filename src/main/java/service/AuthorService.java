d444fc780784e60977af6a8e55612204fa7424c5
arianaaghajani <aghajaniariana@gmail.com>
2024-01-15 11:06:40 +0330

    method of authorService

--- src/main/java/service/AuthorService.java
+++ src/main/java/service/AuthorService.java
@@ -1,8 +1,15 @@
 package service;
 
+
+import repository.AuthorRepository;
+
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class AuthorService {
-    private Scanner scanner=new Scanner(System.in);
+    private AuthorRepository authorRepository=new AuthorRepository();
+    private final Scanner scanner=new Scanner(System.in);
 
+    public AuthorService() throws SQLException {
+    }
 }
