2031ae91532072b350a7654bf528682bdad59fc1
arianaaghajani <aghajaniariana@gmail.com>
2024-01-15 11:47:39 +0330

    fix method of BookService

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -1,5 +1,13 @@
 package service;
 
-public class BookService {
+import repository.BookRepository;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class BookService {
+    private final BookRepository bookRepository=new BookRepository();
+    private final Scanner scanner=new Scanner(System.in);
+    public BookService() throws SQLException {
+    }
 }
