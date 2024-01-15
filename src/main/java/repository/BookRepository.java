9f0b577169d39323c24a21807ed328d217f17652
arianaaghajani <aghajaniariana@gmail.com>
2024-01-15 10:42:26 +0330

    fix method of save book

--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -1,6 +1,8 @@
 package repository;
 
+import java.awt.print.Book;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class BookRepository {
@@ -8,5 +10,16 @@ public class BookRepository {
    private final Connection connection=JdbcConnection.getConnection();
 
     public BookRepository() throws SQLException {
+    }
+    public int save (Book book) throws SQLException{
+        String addAuthor="INSERT INTO books(title,print_Year,authorId)VALUES(?,?,?);";
+        PreparedStatement preparedStatement=connection.prepareStatement(addAuthor);
+        preparedStatement.setString(1,book.getTitle());
+        preparedStatement.setInt(2,book.getPrintYear());
+        preparedStatement.setInt(3,book.getAuthorId);
+        return preparedStatement.executeUpdate();
+
+
+
     }
 }
